aa696c4ffa340cf28488f6f097c9cc1d952b4404
Deepankar Agrawal <deepankar11093@gmail.com>
2017-08-20 21:45:35 +0530

    threshold for movement reduced

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -76,9 +76,9 @@ public class PlayerController : MonoBehaviour {
 
 	public void Move(float inputX) {
 		int deltaX = 0;
-		if (inputX > _transform.position.x + 0.12f) {
+		if (inputX > _transform.position.x + 0.1f) {
 			deltaX = 1;
-		} else if (inputX + 0.12f < _transform.position.x) deltaX = -1;
+		} else if (inputX + 0.1f < _transform.position.x) deltaX = -1;
 		vx = deltaX * maxSpeed;
 
 		_animator.SetFloat("Speed", Mathf.Abs(vx));
