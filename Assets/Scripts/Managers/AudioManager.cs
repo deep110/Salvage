7b668c3a404ae0ec63e004aac9a09461da0bed49
Deepankar Agrawal <deepankar11093@gmail.com>
2018-01-17 14:33:46 +0530

    add AudioManager

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Sound {
+
+	public string name;
+	public AudioClip audioClip;
+
+	[Range(0, 1f)]
+	public float volume = 0.7f;
+
+	[Range(0.5f, 1f)]
+	public float pitch = 1f;
+
+	private AudioSource audioSource;
+
+	public void SetSource(AudioSource source) {
+		audioSource = source;
+		audioSource.volume = volume;
+		audioSource.pitch = pitch;
+	}
+
+	public void Play() {
+		audioSource.Play();
+	}
+}
+
+public class AudioManager : PersistentSingleton <AudioManager> {
+
+	[SerializeField]
+	private Sound[] sounds;
+
+	void Start() {
+		for (int i=0; i < sounds.Length; i++) {
+			var _go = new GameObject("Sound_" + i + "_" + sounds[i].name);
+			_go.transform.SetParent(transform);
+			sounds[i].SetSource(_go.AddComponent<AudioSource>());
+		}
+	}
+
+	public void PlaySound(string name) {
+		for (int i=0; i < sounds.Length; i++) {
+			if (sounds[i].name == name) {
+				sounds[i].Play();
+
+				return;
+			}
+		}
+	}
+}
--- Assets/Scripts/Managers/LocalizationManager.cs
+++ Assets/Scripts/Managers/LocalizationManager.cs
@@ -2,22 +2,34 @@
 using UnityEngine;
 using System.IO;
 
+[System.Serializable]
+public class LocalizationItem {
+    public string key;
+    public string value;
+}
+
+[System.Serializable]
+public class LocalizationData {
+    public LocalizationItem [] items;
+}
+
+
 public class LocalizationManager : PersistentSingleton <LocalizationManager> {
 
 	private Dictionary<string, string> localizedText;
 	private bool isReady;
 
-	public void LoadLocalizedText(string languageCode){
+	public void LoadLocalizedText(string languageName){
 
         localizedText = new Dictionary <string, string> ();
-        string filePath = Path.Combine(Application.streamingAssetsPath, "text_"+languageCode+".json");
+        string filePath = Path.Combine(Application.streamingAssetsPath, "text_"+languageName+".json");
 
-        if (File.Exists (filePath)) {
-            string dataAsJson = File.ReadAllText (filePath);
-            LocalizationData loadedData = JsonUtility.FromJson<LocalizationData> (dataAsJson);
+        if (File.Exists(filePath)) {
+            string dataAsJson = File.ReadAllText(filePath);
+            LocalizationData loadedData = JsonUtility.FromJson<LocalizationData>(dataAsJson);
 
             for (int i = 0; i < loadedData.items.Length; i++){
-                localizedText.Add (loadedData.items [i].key, loadedData.items [i].value);   
+                localizedText.Add(loadedData.items[i].key, loadedData.items[i].value);   
             }
         } else {
             Debug.LogError ("Cannot find file!");
