06aa914c519257230936a4a62d65e84703f388fd
Deepankar Agrawal <deepankar11093@gmail.com>
2018-08-23 22:33:37 +0530

    add icon sprites

--- Assets/Scripts/Managers/AdsManager.cs
+++ Assets/Scripts/Managers/AdsManager.cs
@@ -13,7 +13,7 @@ public class AdsManager : Singleton<AdsManager> {
 
     void Start() {
         if (Advertisement.isSupported) {
-            Advertisement.Initialize(gameId, true);
+            Advertisement.Initialize(gameId, false);
         }
     }
 
